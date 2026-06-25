c1c2aa0cb73326fce69c6e82e16f0b0f633d73de
farhanadeata <fadeatalarik@gmail.com>
2026-06-25 23:04:44 +0700

    Configure GitHub Pages export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
